639b003fd5821aeb8a2e5d3698bde54228fdf77b
Kate <katekim5809@gmail.com>
2019-01-15 19:34:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 function dwarfRollCall(dwarves) {
+  var array = [];
+  for(var i = 0; i < dwarves.length; i++){
+    array.push( [i+1] + ". " + dwarves[i]+ " ")
+  }
+  return array.join('');
 }
 
 function summonCaptainPlanet(planeteerCalls){
-}
-
-function longPlaneteerCalls(words) {
-}
-
-function findTheCheese (foods) {
+  var array =[];
+  for( var i = 0; i< planeteerCalls.length; i++){
+    array.push(planeteerCalls[i].toUpperCase() + " ")
+  }
 }
