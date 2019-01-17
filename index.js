d0650a3cd59d1503e4f477cc62c4bb4dbe271a18
Kate <katekim5809@gmail.com>
2019-01-17 20:21:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(dwarves){
   var array = [];
   for(var i = 0; i < dwarves.length; i++){
-    array.push( [i+1] + ". " + dwarves[i]+ " ")
+    array.push([i+1]+". "+dwarves[i]+" ");
   }
   return array.join('');
 }
 
-function summonCaptainPlanet(planeteerCalls){
-  var array =[];
-  for( var i = 0; i< planeteerCalls.length; i++){
-    array.push(planeteerCalls[i].toUpperCase() + " ")
+
+function summonCaptainPlanet(planeteerCall){
+  var array = [];
+  var i = 0;
+  while(i < planeteerCall.length){
+    array.push(planeteerCall[i].toUpperCase() + "!");
+    i++;
   }
+  return array;
+}
+
+
+
+function longPlaneteerCalls(words){
+  for(var i = 0; i < words.length; i++){
+    if(words[i].length > 4){
+      return true;
+    }
+  }
+  return false;
+}
+
+function findTheCheese(foods){
+  for(var i = 0; i < foods.length; i++){
+  if(foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert"){
+    return foods[i];
+  }
+  }
+  return "no cheese!";
 }
